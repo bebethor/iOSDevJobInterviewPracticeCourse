3193bcf2cc16435f0f1501302b0be8c9c4c6c274
Jose Alberto Ruiz-Carrillo González <jruiz@bancocaminos.es>
2020-02-10 16:42:17 +0100

    SETTING UP: Search view controller

--- GitHubFollowers/GitHubFollowers/SearchViewController.swift
+++ GitHubFollowers/GitHubFollowers/SearchViewController.swift
@@ -10,15 +10,36 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
-    var button = GHFButton()
+    // MARK: - UI -
+    let logoImageView: UIImageView   = UIImageView()
+    let userTextField: GHFTextField  = GHFTextField()
+    let button: GHFButton            = GHFButton(backgroundColor: .systemGreen, title: "Get followers")
     
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
         
-        button = GHFButton(backgroundColor: .systemGreen, title: "ENTER")
-        button = GHFButton(frame: CGRect(x: 0.0, y: 0.0, width: 50.0, height: 100.0))
-        view.addSubview(button)
+        view.backgroundColor = .systemBackground // light mode white / dark mode black
+        configureLogoImageView()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    // MARK: - Configure UI -
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80), // anchor to top (vertical)
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor), // center x anchor. Center horizontally
+            logoImageView.widthAnchor.constraint(equalToConstant: 200), // width
+            logoImageView.heightAnchor.constraint(equalToConstant: 200) // height
+        ])
+        
+    }
 }

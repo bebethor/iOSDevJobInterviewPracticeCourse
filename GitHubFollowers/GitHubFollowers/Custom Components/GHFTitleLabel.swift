f7a4187c10d39e74792b492a7f64e2e39c036f88
Jose Alberto Ruiz-Carrillo González <jruiz@bancocaminos.es>
2020-02-11 11:07:24 +0100

    ADDED AND ORGANIZE: Added new title label file and organize porject in folders

---
+++ GitHubFollowers/GitHubFollowers/Custom Components/GHFTitleLabel.swift
@@ -0,0 +1,21 @@
+//
+//  GHFTitleLabel.swift
+//  GitHubFollowers
+//
+//  Created by Jose Alberto Ruíz-Carrillo González on 11/02/2020.
+//  Copyright © 2020 JARCG. All rights reserved.
+//
+
+import UIKit
+
+class GHFTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // Custom code
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

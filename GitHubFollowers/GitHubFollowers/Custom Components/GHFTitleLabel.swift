473f0002991811e5da31b9f0db2c3e848b57063e
Jose Alberto Ruiz-Carrillo González <jruiz@bancocaminos.es>
2020-02-11 11:27:22 +0100

    ADDED: Logic to title label

--- GitHubFollowers/GitHubFollowers/Custom Components/GHFTitleLabel.swift
+++ GitHubFollowers/GitHubFollowers/Custom Components/GHFTitleLabel.swift
@@ -13,9 +13,25 @@ class GHFTitleLabel: UILabel {
     override init(frame: CGRect) {
         super.init(frame: frame)
         // Custom code
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Custom initializer
+    init(textAligment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+    }
+    
+    private func configure() {
+        textColor                                  = .label
+        adjustsFontSizeToFitWidth                  = true
+        minimumScaleFactor                         = 0.90
+        lineBreakMode                              = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints  = false
     }
 }

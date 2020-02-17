03e3831533019ba469b0874e9f7b461de69d52fe
JARCG <>
2020-02-17 14:27:19 +0100

    ADDED: Logic

--- GitHubFollowers/GitHubFollowers/Custom Components/GHFAlertViewController.swift
+++ GitHubFollowers/GitHubFollowers/Custom Components/GHFAlertViewController.swift
@@ -9,22 +9,33 @@
 import UIKit
 
 class GHFAlertViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: - UI elements -
+    let containerView = UIView() // contains titleLabel, messageLabel & actionButton
+    let titleLabel    = GHFTitleLabel(textAligment: .center, fontSize: 20)
+    let messageLabel  = GHFBodyLabel(textAligment: .center)
+    let actionButton  = GHFButton(backgroundColor: .systemPink, title: "Ok")
+    
+    // MARK: - Properties -
+    var alertTitle: String?
+    var message: String?
+    var buttonTitle: String?
+    
+    // MARK: - Init -
+    init(title: String, message: String, buttonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.alertTitle   = title
+        self.message      = message
+        self.buttonTitle  = buttonTitle
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+    }
 }
--- GitHubFollowers/GitHubFollowers/Custom Components/GHFBodyLabel.swift
+++ GitHubFollowers/GitHubFollowers/Custom Components/GHFBodyLabel.swift
@@ -9,5 +9,29 @@
 import UIKit
 
 class GHFBodyLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // Custom code
+        configure()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Custom initializer
+    init(textAligment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor                                  = .secondaryLabel
+        font                                       = UIFont.preferredFont(forTextStyle: .body) // Dynamic font
+        adjustsFontSizeToFitWidth                  = true
+        minimumScaleFactor                         = 0.75
+        lineBreakMode                              = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints  = false
+    }
 }

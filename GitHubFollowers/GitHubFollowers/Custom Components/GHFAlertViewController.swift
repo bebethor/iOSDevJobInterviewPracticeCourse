ebaf59a8d307cd4038084207512aa83e0dbb7f90
JARCG <>
2020-02-17 14:21:44 +0100

    ADDED: New file GHFAlertViewController

---
+++ GitHubFollowers/GitHubFollowers/Custom Components/GHFAlertViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GHFAlertViewController.swift
+//  GitHubFollowers
+//
+//  Created by Jose Alberto on 17/02/2020.
+//  Copyright © 2020 JARCG. All rights reserved.
+//
+
+import UIKit
+
+class GHFAlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

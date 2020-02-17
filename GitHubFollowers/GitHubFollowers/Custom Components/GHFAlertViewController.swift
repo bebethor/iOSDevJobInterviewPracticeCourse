e24b1a9fe72da119dd7d1a65735dbd2fc2d9e656
JARCG <>
2020-02-17 15:17:13 +0100

    ADDED : Logic

--- GitHubFollowers/GitHubFollowers/Custom Components/GHFAlertViewController.swift
+++ GitHubFollowers/GitHubFollowers/Custom Components/GHFAlertViewController.swift
@@ -37,5 +37,33 @@ class GHFAlertViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        
+        configureContainerView()
+        configureTitleLabel()
+    }
+    
+    // MARK: - Configure UI -
+    func configureContainerView() {
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.layer.cornerRadius = 16
+        containerView.layer.borderColor = UIColor.white.cgColor
+        NSLayoutConstraint.activate([
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor), // Center vertically
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 280),
+            containerView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+    }
+    
+    func configureTitleLabel() {
+        containerView.addSubview(titleLabel)
+        titleLabel.text = alertTitle ?? "Something went wrong"
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            titleLabel.heightAnchor.constraint(equalToConstant: 28)
+        ])
     }
 }

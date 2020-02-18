54ae916002248b73c0049b5004aa0b95883438fc
JARCG <>
2020-02-18 13:38:53 +0100

    ADDED: Logic

--- GitHubFollowers/GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/GitHubFollowers/Managers/NetworkManager.swift
@@ -11,25 +11,42 @@ import Foundation
 class NetworkManager {
     // Singleton
     static let shared   = NetworkManager() //  this information will be shared among all the instances
-    let baseURL         = "https://api.github.com/users"
+    let baseURL         = "https://api.github.com/users/"
     
     private init() {}
     
     func getFollowers(for username: String, page: Int, completion: @escaping ([Follower]?, String?) -> Void) {
-        let endpoint = baseURL + "/users/\(username)/followers?per_page=100&page=\(page)"
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         guard let url = URL(string: endpoint) else {
             completion(nil, "This username does not exist. Please try with another user name") // nil is for the followers array
             return
         }
         
+        print(url)
+        
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if let _ = error {
-                completion(nil, "Unable to complete request.")
+                completion(nil, "Unable to complete request. Invalid URL")
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                
+                completion(nil, "Invalid response from server")
                 return
+            }
+            
+            guard let data = data else {
+                completion(nil, "Invalid data received from server")
+                return
+            }
+            
+            // Parse JSON
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completion(followers, nil)
+            } catch {
+                completion(nil, "Invalid data received from server")
             }
         }
         
--- GitHubFollowers/GitHubFollowers/Scenes/FollowersViewController.swift
+++ GitHubFollowers/GitHubFollowers/Scenes/FollowersViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class FollowersViewController: UIViewController {
 
-    var userName: String?
+    var userName: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor                                    = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles  = true
+        
+        NetworkManager.shared.getFollowers(for: userName, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else {
+                self.presentGHFAlertOnMainThreat(title: "Something went wrong", message: errorMessage!, buttonTitle: "OK")
+                return
+            }
+            
+            print("Followers count: \(followers.count)")
+            print(followers)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {

2fee42deb0e245b7365fd74d16edbc6d6bff18e7
JARCG <>
2020-02-20 12:03:48 +0100

    ADDED: UIHelper swift file

---
+++ GitHubFollowers/GitHubFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,14 @@
+//
+//  UIHelper.swift
+//  GitHubFollowers
+//
+//  Created by Jose Alberto on 20/02/2020.
+//  Copyright © 2020 JARCG. All rights reserved.
+//
+
+import Foundation
+
+struct UIHelper {
+    
+    
+}

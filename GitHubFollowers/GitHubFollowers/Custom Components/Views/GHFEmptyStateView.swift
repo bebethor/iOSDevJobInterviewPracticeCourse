fbf05809c59806da306f0028de9198ff69b07b08
JARCG <>
2020-02-21 14:44:48 +0100

    ADDED: New file GHFEmptyStateView

---
+++ GitHubFollowers/GitHubFollowers/Custom Components/Views/GHFEmptyStateView.swift
@@ -0,0 +1,13 @@
+//
+//  GHFEmptyStateView.swift
+//  GitHubFollowers
+//
+//  Created by Jose Alberto on 21/02/2020.
+//  Copyright © 2020 JARCG. All rights reserved.
+//
+
+import UIKit
+
+class GHFEmptyStateView: UIView {
+    
+}

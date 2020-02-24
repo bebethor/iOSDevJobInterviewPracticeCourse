214db7db8e9525beabfa1c83db4849c2540b36a0
JARCG <>
2020-02-24 18:03:17 +0100

    ADDED: New file UserInfoViewController

---
+++ GitHubFollowers/GitHubFollowers/Scenes/UserInfViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserInfViewController.swift
+//  GitHubFollowers
+//
+//  Created by Jose Alberto on 24/02/2020.
+//  Copyright © 2020 JARCG. All rights reserved.
+//
+
+import UIKit
+
+class UserInfViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}

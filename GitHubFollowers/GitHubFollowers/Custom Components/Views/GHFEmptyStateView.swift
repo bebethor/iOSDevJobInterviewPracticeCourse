5b78d6e61c5b2c307880e8546ae3600b8c9618f8
JARCG <>
2020-02-24 12:42:14 +0100

    ADDED: Logic

--- GitHubFollowers/GitHubFollowers/Custom Components/Views/GHFEmptyStateView.swift
+++ GitHubFollowers/GitHubFollowers/Custom Components/Views/GHFEmptyStateView.swift
@@ -9,18 +9,32 @@
 import UIKit
 
 class GHFEmptyStateView: UIView {
-    let messageLabel = GHFTitleLabel(textAlignment: .center, fontSize: 20)
-    let imageView = GHFImageView(frame: .zero)
+    let messageLabel = GHFTitleLabel(textAlignment: .center, fontSize: 28)
+    let logoImageView = UIImageView()
     
     // MARK: - Custom inits -
     override init(frame: CGRect) {
         super.init(frame: frame)
         // Custom code here
+        configure()
     }
     // Story board init
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func configure() {
+        addSubview(messageLabel)
+        addSubview(logoImageView)
+    }
     
+    private func configureMessageLabel() {
+        messageLabel.numberOfLines   = 3
+        messageLabel.textColor       = .secondaryLabel
+    }
+    
+    private func configureLogoImageView() {
+        logoImageView.image                                     = UIImage(named: "empty-state-logo")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+    }
 }

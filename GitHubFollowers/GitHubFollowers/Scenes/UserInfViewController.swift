08b08ad08437ee1adf0946878015ccd62a8c4af8
JARCG <>
2020-02-25 14:46:54 +0100

    ADDED: get user call in UserInfoViewController for test

--- GitHubFollowers/GitHubFollowers/Scenes/UserInfViewController.swift
+++ GitHubFollowers/GitHubFollowers/Scenes/UserInfViewController.swift
@@ -19,9 +19,43 @@ class UserInfViewController: UIViewController {
         title = username
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dissmisVC))
         navigationItem.rightBarButtonItem = doneButton
+        
+        NetworkManager.shared.getUserInfo(for: username) { [ weak self] (result) in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGHFAlertOnMainThreat(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
     
     @objc func dissmisVC() {
         dismiss(animated: true, completion: nil)
     }
+    
+//    func getFollowers(username: String, page: Int) {
+//        showLoading()
+//        NetworkManager.shared.getFollowers(for: userName, page: page) { [weak self] (result) in
+//            guard let self = self else { return }
+//            self.dismissLoading()
+//            switch result {
+//            case .success(let followers):
+//                if followers.count < 100 { self.hasMoreFollowers = false }
+//                self.followers.append(contentsOf: followers)
+//
+//                let message = "This user doesn´t have followers. Go follow this user!!"
+//                if self.followers.isEmpty {
+//                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
+//                    return
+//                }
+//
+//                self.updateData(on: self.followers)
+//            case .failure(let error):
+//                self.presentGHFAlertOnMainThreat(title: "Something went wrong", message: error.rawValue, buttonTitle: "OK")
+//            }
+//        }
+//    }
 }

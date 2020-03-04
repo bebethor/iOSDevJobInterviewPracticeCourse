a3fd9aeda6d6e869611abf88feb5bb87274206b2
Jose Alberto Ruíz-Carrillo González <jose.alberto.rcg@gmail.com>
2020-03-04 12:04:26 +0100

    REFACTOR

---
+++ GitHubFollowers/GitHubFollowers/Scenes/UserInfoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UserInfoViewController.swift
+//  GitHubFollowers
+//
+//  Created by Jose Alberto on 24/02/2020.
+//  Copyright © 2020 JARCG. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        title = username
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dissmisVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        NetworkManager.shared.getUserInfo(for: username) { [ weak self] (result) in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGHFAlertOnMainThreat(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+    
+    @objc func dissmisVC() {
+        dismiss(animated: true, completion: nil)
+    }
+}

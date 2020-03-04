0dee1fd0e1a4e9191950d6a811f9f96f43a76fcf
Jose Alberto Ruíz-Carrillo González <jose.alberto.rcg@gmail.com>
2020-03-04 17:40:28 +0100

    ADDED: GHFUserInfoHeaderViewController file

---
+++ GitHubFollowers/GitHubFollowers/Custom Components/View Controllers/GHFUserInfoHeaderViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GHFUserInfoHeaderViewController.swift
+//  GitHubFollowers
+//
+//  Created by Jose Alberto Ruíz-Carrillo González on 04/03/2020.
+//  Copyright © 2020 JARCG. All rights reserved.
+//
+
+import UIKit
+
+class GHFUserInfoHeaderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}

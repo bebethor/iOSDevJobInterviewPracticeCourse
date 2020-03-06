f0ab97213ea6254839933609d91bb234bcf0deb7
Jose Alberto Ruíz-Carrillo González <jose.alberto.rcg@gmail.com>
2020-03-06 14:38:40 +0100

    ADDED: Configure UI Logic

--- GitHubFollowers/GitHubFollowers/Custom Components/Views/GHFItemInfoView.swift
+++ GitHubFollowers/GitHubFollowers/Custom Components/Views/GHFItemInfoView.swift
@@ -16,6 +16,9 @@ class GHFItemInfoView: UIView {
     let titleLabel          = GHFTitleLabel(textAlignment: .left, fontSize: 14)
     let countingReposLabel  = GHFTitleLabel(textAlignment: .center, fontSize: 14)
     
+    // MARK: - Properties -
+    var uiItemsArray     = [UIView]()
+    
     // MARK: - Inits -
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,9 +29,47 @@ class GHFItemInfoView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Configure Functions -
     func configure() {
+        uiItemsArray = [symbolImageView, titleLabel, countingReposLabel]
         
+        for item in uiItemsArray {
+            addSubview(item)
+            item.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        configureSymbolImageView()
+        configureTitleLabel()
+        configureCountingReposLabel()
     }
     
+    func configureSymbolImageView() {
+        symbolImageView.contentMode = .scaleAspectFill
+        symbolImageView.tintColor   = .label
+        
+        NSLayoutConstraint.activate([
+            symbolImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            symbolImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            symbolImageView.heightAnchor.constraint(equalToConstant: 20),
+            symbolImageView.widthAnchor.constraint(equalToConstant: 20)
+        ])
+    }
     
+    func configureTitleLabel() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: symbolImageView.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: symbolImageView.leadingAnchor, constant: 12),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 18)
+        ])
+    }
+    
+    func configureCountingReposLabel() {
+        NSLayoutConstraint.activate([
+            countingReposLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
+            countingReposLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            countingReposLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            countingReposLabel.heightAnchor.constraint(equalToConstant: 18)
+        ])
+    }
 }

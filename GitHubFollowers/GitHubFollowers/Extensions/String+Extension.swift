7da005ddfec2f33e6d66bb35d71a23c9b492c319
Jose Alberto Ruíz-Carrillo González <jose.alberto.rcg@gmail.com>
2020-03-10 10:49:56 +0100

    ADDED: file and logic

---
+++ GitHubFollowers/GitHubFollowers/Extensions/String+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  String+Extension.swift
+//  GitHubFollowers
+//
+//  Created by Jose Alberto Ruíz-Carrillo González on 10/03/2020.
+//  Copyright © 2020 JARCG. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func convertToDate() -> Date {
+        
+        let dateFormatter           = DateFormatter()
+        dateFormatter.dateFormat    = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.locale        = Locale(identifier: "es_ES")
+        dateFormatter.timeZone      = .current
+        
+        return Date()
+    }
+}

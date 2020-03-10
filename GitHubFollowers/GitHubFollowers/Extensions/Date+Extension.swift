36708d26033010c4e692b9888e4ddc27c1c13e86
Jose Alberto Ruíz-Carrillo González <jose.alberto.rcg@gmail.com>
2020-03-10 09:11:13 +0100

    ADDED: New file Date+Extension

---
+++ GitHubFollowers/GitHubFollowers/Extensions/Date+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Date+Extension.swift
+//  GitHubFollowers
+//
+//  Created by Jose Alberto Ruíz-Carrillo González on 10/03/2020.
+//  Copyright © 2020 JARCG. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+}

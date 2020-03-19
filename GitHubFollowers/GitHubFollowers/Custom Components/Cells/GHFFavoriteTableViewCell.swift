711e5e72b084254b29b17b64a37d016d393dde60
Jose Alberto Ruíz-Carrillo González <jose.alberto.rcg@gmail.com>
2020-03-19 13:28:03 +0100

    ADDED: New favorite cell and logic

---
+++ GitHubFollowers/GitHubFollowers/Custom Components/Cells/GHFFavoriteTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  GHFFavoriteTableViewCell.swift
+//  GitHubFollowers
+//
+//  Created by Jose Alberto Ruíz-Carrillo González on 19/03/2020.
+//  Copyright © 2020 JARCG. All rights reserved.
+//
+
+import UIKit
+
+class GHFFavoriteTableViewCell: UITableViewCell {
+
+    // MARK: - UI Elements -
+    static let reuseID   = "FavoritCell"
+    let avatarImageView  = GHFImageView(frame: .zero)
+    let userNameLabel    = GHFTitleLabel(textAlignment: .center, fontSize:216)
+    
+    
+    
+    
+}

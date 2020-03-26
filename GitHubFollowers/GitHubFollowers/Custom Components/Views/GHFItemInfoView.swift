dda91d2bb9c27050ea27b005eee29e02e9fc7379
Jose Alberto Ruíz-Carrillo González <jose.alberto.rcg@gmail.com>
2020-03-26 15:47:58 +0100

    REFACTOR

--- GitHubFollowers/GitHubFollowers/Custom Components/Views/GHFItemInfoView.swift
+++ GitHubFollowers/GitHubFollowers/Custom Components/Views/GHFItemInfoView.swift
@@ -78,16 +78,16 @@ class GHFItemInfoView: UIView {
     func set(infoItemType: infoItemType, withCount count: Int) {
         switch infoItemType {
         case .repos:
-            symbolImageView.image = UIImage(systemName: SFSymbols.repos)
+            symbolImageView.image = SFSymbols.repos
             titleLabel.text       = "Public Repos"
         case .gists:
-            symbolImageView.image = UIImage(systemName: SFSymbols.gists)
+            symbolImageView.image = SFSymbols.gists
             titleLabel.text       = "Public Gists"
         case .followers:
-            symbolImageView.image = UIImage(systemName: SFSymbols.followers)
+            symbolImageView.image = SFSymbols.followers
             titleLabel.text       = "Followers"
         case .following:
-            symbolImageView.image = UIImage(systemName: SFSymbols.following)
+            symbolImageView.image = SFSymbols.following
             titleLabel.text       = "Following"
         }
         countLabel.text = String(count)
